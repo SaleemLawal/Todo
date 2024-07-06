b4e8e9242e586652286537e4e450ed871ff91a18
SaleemLawal <slawal1@umbc.edu>
2024-07-06 14:55:40 -0400

    formatted files

--- Frontend/todo/src/App.tsx
+++ Frontend/todo/src/App.tsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import TodoList from "./components/TodoList";
 import NavBar from "./components/NavBar";
-// import TodoDetail from "./components/TodoDetail";
-import AddTodo from "./components/AddTodo";
+import AddTodoModal from "./components/AddTodoModal";
 import { useRef, useState } from "react";
 import TodoDetail from "./components/TodoDetail";
 
@@ -18,44 +17,46 @@ const App = () => {
     <section id="main-page">
       <BrowserRouter>
         <NavBar />
-        <main className=" ml-96 mt-12">
+        <main className="mt-12 ml-96">
           <Routes>
             <Route path="/" element={<Navigate to={"/todos"} />} />
             <Route
               path="/todos"
               element={<TodoList toggleModal={toggleModal} />}
             />
-            <Route
-              path="/todos/:id"
-              element={<TodoDetail />}
-            />
+            <Route path="/todos/:id" element={<TodoDetail />} />
             <Route path="/todos/add-todo" element={<TodoDetail />} />
             <Route
               path="/todos/search"
-              element={<h1 className="text-4xl font-bold mb-6">Search Todo</h1>}
+              element={<h1 className="mb-6 text-4xl font-bold">Search Todo</h1>}
             />
             <Route
               path="/todos/today"
-              element={<h1 className="text-4xl font-bold mb-6">Today</h1>}
+              element={<h1 className="mb-6 text-4xl font-bold">Today</h1>}
             />
             <Route
               path="/todos/upcoming"
-              element={<h1 className="text-4xl font-bold mb-6">Upcoming</h1>}
+              element={<h1 className="mb-6 text-4xl font-bold">Upcoming</h1>}
             />
             <Route
               path="/todos/tags"
-              element={<h1 className="text-4xl font-bold mb-6">Tags</h1>}
+              element={<h1 className="mb-6 text-4xl font-bold">Tags</h1>}
             />
           </Routes>
         </main>
 
         {isOpen && <div className="overlay"></div>}
-        <dialog ref={modalRef} className="modal shadow-inner">
+        <dialog ref={modalRef} className="shadow-inner modal">
           <div className="flex flex-row">
-            <span className=" text-xl font-semibold text-center flex-1">New Todo</span>
-            <i onClick={() => toggleModal(false)} className="bi bi-x-circle-fill text-lg text-gray-400"></i>
+            <span className="flex-1 text-xl font-semibold text-center ">
+              New Todo
+            </span>
+            <i
+              onClick={() => toggleModal(false)}
+              className="text-lg text-gray-400 bi bi-x-circle-fill"
+            ></i>
           </div>
-          <AddTodo />
+          <AddTodoModal toggleModal = {toggleModal}/>
         </dialog>
       </BrowserRouter>
     </section>
