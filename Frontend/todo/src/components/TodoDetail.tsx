6afee14796b32b81dad7b851609d0d723199fb3d
SaleemLawal <slawal1@umbc.edu>
2024-07-06 14:58:37 -0400

    updated component to render specific todo detail

--- Frontend/todo/src/components/TodoDetail.tsx
+++ Frontend/todo/src/components/TodoDetail.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getTodoById } from "../util/api";
+import AddTodoModal from "./AddTodoModal";
+import { TodoData } from "../util/interface";
 
 const TodoDetail: React.FC = () => {
   const { id } = useParams();
-  return <h1>{id}</h1>;
+  const [todo, setTodo] = useState<TodoData>({
+    name: "",
+    description: "",
+    status: "PENDING",
+    priority: "LOW",
+    dueDate: "",
+    tags: [],
+    completed: false,
+  })
+
+  const getTodo = useCallback(async () => {
+    if (id){
+      try {
+        const {data} = await getTodoById(id)
+        const formattedDate = data.dueDate ? new Date(data.dueDate).toISOString().split('T')[0] : "";
+        setTodo({
+          ...data, dueDate: formattedDate
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }, [id])
+  
+  useEffect(() => {
+    getTodo()
+  }, [getTodo]);
+  
+  return (
+    
+    <>
+      <p className="mb-6 text-4xl font-bold">{id ? `Edit Todo (${id})` : "Add Todo"}</p>
+      <AddTodoModal data = {todo}/>
+    </>
+  );
 };
 
 export default TodoDetail;
