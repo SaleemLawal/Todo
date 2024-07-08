55d227a01afc2f1285e6a5481495082d0c8cd633
SaleemLawal <slawal1@umbc.edu>
2024-07-08 11:37:01 -0400

    Implement toggling completed to true when completed button is clicked

--- Frontend/todo/src/components/TodoElement.tsx
+++ Frontend/todo/src/components/TodoElement.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { TodoData } from "../util/interface";
+import { saveTodo } from "../util/api";
 
 interface Props {
   data: TodoData;
 }
 
 const TodoElement: React.FC<Props> = ({ data }) => {
+  const navigate = useNavigate();
   const { id, name: title, description, priority, dueDate } = data;
 
-  const handleClick = (event: React.MouseEvent) => {
+  const handleClick = async (event: React.MouseEvent) => {
     event.preventDefault();
-    console.log("clicked todo with id", id);
+    const updatedTodoItem = {
+      ...data,
+      completed: true,
+    };
+    await saveTodo(updatedTodoItem);
+    navigate(0);
   };
   let priorityClass = "text-sm text-gray-500 px-1 rounded ";
   if (priority === "LOW") {
@@ -23,21 +30,36 @@ const TodoElement: React.FC<Props> = ({ data }) => {
   }
   return (
     <Link to={`/todos/${id}`}>
-      <div className="flex gap-4 items-start border-b-2 border-gray-200 p-2">
+      <div className="flex items-start gap-4 p-2 border-b-2 border-gray-200">
         <button
-          className="border-2 border-gray-400 rounded-full w-6 h-6 mt-1"
+          className="w-6 h-6 mt-1 border-2 border-gray-400 rounded-full"
           onClick={handleClick}
         ></button>
         <div className="flex-1">
           <p className="text-xl font-semibold">{title}</p>
-          <p className="text-base text-gray-400 font-light">{description}</p>
+          <p className="text-base font-light text-gray-400">{description}</p>
           <div className="flex items-center mt-2">
             <p className="text-sm text-gray-500">
               {new Date(dueDate).toLocaleDateString()}
             </p>
-            <span className="text-sm text-gray-500 mx-2">•</span>
+            <span className="mx-2 text-sm text-gray-500">•</span>
             <p className={priorityClass}>{priority}</p>
           </div>
+          {data.tags.length > 0 && (
+            <p className="mt-1 ">
+              <i className="mr-1 bi bi-tags"></i>
+              {data.tags.map((tag, index) => (
+                <span
+                  key={tag}
+                  className={`px-2 mr-2  rounded ${
+                    index % 2 === 0 ? "bg-yellow-200" : "bg-teal-300"
+                  }`}
+                >
+                  {tag}
+                </span>
+              ))}
+            </p>
+          )}
         </div>
       </div>
     </Link>
